599cf75f2c0fc972d061b6112e8eb72c13b6482d
JordyC++n <jcuan4@gmail.com>
2015-07-16 20:53:00 -0500

    Pequeños errores en la DB corregidos

--- models/models.js
+++ models/models.js
@@ -3,26 +3,27 @@ var path = require('path');
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize(null, null, null,
+// Usar BBDD SQLite:
+var sequelize = new Sequelize(null, null, null, 
 						{dialect: "sqlite", storage: "quiz.sqlite"}
 					);
 
-// Importar la definición de la tabla Quiz en quiz.js
+// Importar la definicion de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
-exports.Quiz = Quiz; // Exportar la definición de tabla Quiz
+exports.Quiz = Quiz; // exportar definición de tabla Quiz
 
-// sequelize.sync() crea e inicializa la tabla de preguntas en DB
-sequelize.sync().success(
+// sequelize.sync() crea e inicializa tabla de preguntas en DB
+sequelize.sync().then(function() {
 	// success(..) ejecuta el manejador una vez creada la tabla
-	Quiz.count().success( function (count) {
-		if (count === 0) {
-			Quiz.create({
-				pregunta : 'Capital de Italia',
+	Quiz.count().then(function (count){
+		if(count === 0) {   // la tabla se inicializa solo si está vacía
+			Quiz.create({ 
+				pregunta: 'Capital de Italia',
 				respuesta: 'Roma'
-			}).success(function() {
-				console.log('BDs inicializada');
-			});
-		}	
-	})
-);
+			})
+			.success(function(){console.log('Base de datos inicializada')});
+		}
+	});
+	console.log(" ** Jordy *** Se ha consultado");
+});
