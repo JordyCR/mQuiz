16c2217a8f07cf9cf2a58d297dcb78ed7bad31ae
JordyC++n <jcuan4@gmail.com>
2015-08-05 14:38:12 -0500

    Moderación de comentarios

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,14 +2,14 @@ var models = require('../models/models.js');
 
 // Autoload :id
 exports.load = function(req, res, next, quizId) {
-models.Quiz.find({
-					where: {
-						id: Number(quizId)
-					},
-					include: [{
-						model: models.Comment
-					}]
-				}).then(function(quiz) {
+	models.Quiz.find({
+		where: {
+			id: Number(quizId)
+		},
+		include: [{
+			model: models.Comment
+		}]
+	}).then(function(quiz) {
 			if (quiz) {
 				req.quiz = quiz;
 				next();
